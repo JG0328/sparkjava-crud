f8454a45b293994d13e9920403fafda0731ef084
Jose Gonzalez <jgg032899@gmail.com>
2019-11-28 11:20:20 -0400

    Puerto para Heroku

--- src/main/java/edu/pucmm/sparkjavacrud/Main.java
+++ src/main/java/edu/pucmm/sparkjavacrud/Main.java
@@ -1,17 +1,21 @@
 package edu.pucmm.sparkjavacrud;
-
 import edu.pucmm.sparkjavacrud.controladores.EstudiantesControlador;
-import edu.pucmm.sparkjavacrud.encapsulacion.Estudiante;
-import edu.pucmm.sparkjavacrud.servicios.EstudianteServicio;
-
-import java.util.ArrayList;
-
-import static spark.Spark.staticFiles;
+import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+
         staticFiles.location("/publico");
 
         new EstudiantesControlador().aplicarRutas();
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
     }
 }
